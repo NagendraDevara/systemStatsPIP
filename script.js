3814a1c68ceb6d51a474c1b9a7b80370d8afe9c4
Nagendra Devara <nagendradevara4@gmail.com>
2022-06-07 22:26:20 +0530

    firstcomit

---
+++ script.js
@@ -0,0 +1,26 @@
+const target = document.getElementById('target');
+const source = document.createElement('canvas');
+const ctx = source.getContext('2d');
+ctx.font = "50px Arial";
+ctx.textAlign = "center";
+ctx.textBaseline = "middle";
+anim();
+
+const stream = source.captureStream();
+target.srcObject = stream;
+
+const btn = document.getElementById('btn');
+if( target.requestPictureInPicture ) {
+  btn.onclick = e => target.requestPictureInPicture();
+}
+else {
+  btn.disabled = true;
+}
+
+function anim() {
+  ctx.fillStyle = "white";
+  ctx.fillRect( 0, 0, source.width, source.height );
+  ctx.fillStyle = "black";
+  ctx.fillText( new Date().toTimeString().split(' ')[0], source.width / 2, source.height / 2 );
+  requestAnimationFrame( anim );
+}
